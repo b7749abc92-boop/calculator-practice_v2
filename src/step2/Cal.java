5857ba5762198feb48db1f73bd2bc229be32b3ea
b7749abc92-boop <b7749abc92@gmail.com>
2026-06-17 10:43:04 +0900

    완성

--- src/step2/Cal.java
+++ src/step2/Cal.java
@@ -1,4 +1,29 @@
 package step2;
 
+import java.util.ArrayList;
+
 public class Cal {
+
+    private final ArrayList<Double> rl = new ArrayList<>();
+
+    private double ca(int n1, char a, int n2) {
+
+        double s = 0;
+
+        switch (a) {
+            case '+' -> s = n1 + n2;
+            case '-' -> s = n1 - n2;
+            case '*' -> s = n1 * n2;
+            case '/' -> s = (double) n1 / n2;
+        }
+        rl.add(s);
+        return s;
+    }
+    public  double getca(int n1, char a, int n2) {
+        return ca(n1, a, n2);
+    }
+
+    public ArrayList<Double> getrl() {
+        return rl;
+    }
 }
--- src/step2/Main.java
+++ src/step2/Main.java
@@ -1,4 +1,63 @@
 package step2;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Cal c = new Cal();
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("첫 번째 숫자를 입력해주세요:");
+            if (!sc.hasNextInt()) {
+                System.out.println("숫자만 입력 가능합니다.");
+                sc.next();
+                continue;
+            }
+            int n1 = sc.nextInt();
+            if (n1 < 0) {
+                System.out.println("음수는 입력이 불가합니다.");
+                continue;
+            }
+
+            System.out.println("연산자(+-*/)를 입력해주세요:");
+            char a = sc.next().charAt(0);
+            if (!(a == '+' || a == '-' || a == '*' || a == '/')) {
+                System.out.println("잘못된 입력입니다.");
+                continue;
+            }
+
+            System.out.println("두 번째 숫자를 입력해주세요:");
+            if (!sc.hasNextInt()) {
+                System.out.println("숫자만 입력 가능합니다.");
+                sc.next();
+                continue;
+            }
+            int n2 = sc.nextInt();
+            if (n2 < 0) {
+                System.out.println("음수는 입력이 불가합니다.");
+                continue;
+            }
+            if (a == '/' && n2 == 0) {
+                System.out.println("0으로는 나눌 수 없습니다.");
+                continue;
+            }
+
+           double r = c.getca(n1, a, n2);
+
+            System.out.println("계산 결과: " + n1 + a + n2 + " = " + r);
+
+            ArrayList<Double> fHr = c.getrl();
+            System.out.println("계산기록: " + fHr);
+
+            System.out.println("종료를 원하시면 'exit', 계속 하신다면 아무 글자를 입력해주세요.");
+            String exit = sc.next();
+            if (exit.equals("exit")) {
+                System.out.println("종료합니다");
+                break;
+            }
+        }
+
+    }
 }
